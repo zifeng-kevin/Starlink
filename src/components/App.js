d163434437ece1a81aa79d3e5c9afe38e8dbc227
zifeng-kevin <kavenpig008@gmail.com>
2020-07-25 15:00:26 -0400

    delete unused import

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Header from './Header';
 import Footer from "./Footer";
 import Main from "./Main";
-import SatSetting from "./SatSetting";
 
 function App() {
     return (
